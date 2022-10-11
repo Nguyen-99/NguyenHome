d2a39114f797c409edfb8d1841d4791f949cbb47
jen <nguyenkhoinguyen139@gmail.com>
2022-10-11 18:19:01 +0700

    first implement service

---
+++ src/main/java/com/nguyenz/serviceimpl/RoomServiceImpl.java
@@ -0,0 +1,38 @@
+package com.nguyenz.serviceimpl;
+
+import com.nguyenz.dto.RoomDTO;
+import com.nguyenz.service.RoomService;
+
+import java.util.List;
+
+public class RoomServiceImpl implements RoomService {
+    @Override
+    public List<RoomDTO> getAll() {
+        return null;
+    }
+
+    @Override
+    public RoomDTO getById(int id) {
+        return null;
+    }
+
+    @Override
+    public RoomDTO add(RoomDTO roomDTO) {
+        return null;
+    }
+
+    @Override
+    public RoomDTO update(RoomDTO roomDTO) {
+        return null;
+    }
+
+    @Override
+    public int delete(int id) {
+        return 0;
+    }
+
+    @Override
+    public void hardDelete(int id) {
+
+    }
+}
