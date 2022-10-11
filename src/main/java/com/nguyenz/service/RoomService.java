3b21a60747557de61d5c51583d019e70a76cc40b
jen <nguyenkhoinguyen139@gmail.com>
2022-10-11 17:49:40 +0700

    interface RoomService

--- src/main/java/com/nguyenz/service/RoomService.java
+++ src/main/java/com/nguyenz/service/RoomService.java
@@ -7,8 +7,8 @@ import com.nguyenz.dto.RoomDTO;
 public interface RoomService {
 	List<RoomDTO> getAll();
 	RoomDTO getById(int id);
-	
 	RoomDTO add(RoomDTO roomDTO);
 	RoomDTO update(RoomDTO roomDTO);
-	void delete(int id);
+	int delete(int id);
+	void hardDelete(int id);
 }
